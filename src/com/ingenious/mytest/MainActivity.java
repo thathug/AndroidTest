d410648e75594f9db916b0b932102a8f40756862
Chinasa <c.ndukwe@ingeniousafrica.com>
2012-08-21 22:52:57 +0200

    add my modification

--- src/com/ingenious/mytest/MainActivity.java
+++ src/com/ingenious/mytest/MainActivity.java
@@ -10,6 +10,8 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+       // TextView myTestView = (TextView)getViewById(R)
     }
 
     @Override
